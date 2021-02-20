797426dfffbd04c71d0026490871ee5f9c5749d2
Chris Taylor <chris_taylor_za@hotmail.com>
2021-02-20 18:59:29 -0500

    webusb sample

---
+++ WebUSBApp/WebUSBApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using GHIElectronics.TinyCLR.Devices.UsbClient;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace WebUSBApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var usbclientController = UsbClientController.GetDefault();
+
+            var usbClientSetting = new UsbClientSetting()
+            {
+                Mode = UsbClientMode.WinUsb,
+                ManufactureName = "GHI Electronics",
+                ProductName = "Test WebUSB",
+                SerialNumber = "12345678",
+                Guid = "{F99BAABA-F033-483D-AFB2-A6E4F3AE6310}",
+                VendorId = 0x1209,
+                ProductId = 0xa800,
+                BcdUsb = 0x210,
+            };
+
+            var winUsb = new WinUsb(usbclientController, usbClientSetting);
+            winUsb.DeviceStateChanged += (a, b) => Debug.WriteLine("Connection changed.");
+            winUsb.DataReceived += (a, count) => Debug.WriteLine("Data received:" + count);
+
+            winUsb.Enable();
+
+
+            while (winUsb.DeviceState != DeviceState.Configured) ;
+            Debug.WriteLine("UsbClient Connected");
+
+            while (true)
+            {
+                if (winUsb.Stream.BytesToRead > 0)
+                {
+                    var messageLength = winUsb.Stream.ReadByte();
+                    if (messageLength > 0)
+                    {
+                        var messageBuffer = new byte[messageLength];
+                        var offset = 0;
+                        while (offset < messageLength)
+                        {
+                            var bytesRead = winUsb.Stream.Read(messageBuffer, offset, messageLength - offset);
+                            if (bytesRead > 0)
+                            {
+                                offset += bytesRead;
+                            }
+                        }
+
+                        winUsb.Stream.WriteByte((byte)messageLength);
+                        winUsb.Stream.Write(messageBuffer);
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ web/webusb.js
@@ -0,0 +1,51 @@
+class WebUSB {
+    constructor(inEndpoint, outEndpoint){
+        this.outEndpoint = outEndpoint;
+        this.inEndpoint = inEndpoint;
+    }
+    
+    async connect(deviceFilters) {
+        this.device = await navigator.usb.requestDevice({
+            filters:deviceFilters
+        });
+        
+        await this.device.open();
+        await this.device.selectConfiguration(1);
+        await this.device.claimInterface(0);
+    }
+
+    async sendString(message) {
+        let encoder = new TextEncoder();
+        let bytes = encoder.encode(message); 
+        await this.device.transferOut(this.outEndpoint, new Uint8Array([message.length]));
+        await this.device.transferOut(this.outEndpoint, bytes);
+    }
+
+    async readString() {
+        let messageLengthBuffer = await this.readBytes(1);
+        let messageLength = messageLengthBuffer[0];
+
+        let messageBuffer = await this.readBytes(messageLength);
+        let decoder = new TextDecoder();
+        return decoder.decode(messageBuffer);
+    }
+
+    async readBytes(count) {
+        let buffer = new Uint8Array(count);
+        let offset = 0;
+        while (count > 0) {
+            let result = await this.device.transferIn(this.inEndpoint, count);
+            if (result.data) {
+                buffer.set(new Uint8Array(result.data.buffer), offset);
+                offset += result.data.byteLength; 
+                count -= result.data.byteLength;
+            }
+
+            if (result.status === 'stall') {
+                await this.device.clearHalt(2);
+            }
+        }
+
+        return buffer;
+    }
+}
